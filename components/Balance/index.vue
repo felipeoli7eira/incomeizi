c3d4fd132a35caaa30347c6e49c12b4a9a7439d6
felipeoli7eira <me.felipeoliveira@gmail.com>
2024-11-11 17:48:27 -0300

    build

--- components/Balance/index.vue
+++ components/Balance/index.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="flex items-stretch space-x-1">
-        <h1 class="text-lg p-2 relative">
-            <client-only>
+    <ClientOnly>
+        <div class="flex items-stretch space-x-1">
+            <h1 class="text-lg p-2 relative">
                 <p>Saldo: {{ formatToCurrency(expensesStore.balance) }}</p>
-            </client-only>
 
-            <div v-if="!displayBalance" class="backdrop-blur-sm h-full rounded-md bg-white/30 absolute top-0 left-0 right-0 bottom-0"></div>
-        </h1>
+                <div v-if="!displayBalance" class="backdrop-blur-sm h-full rounded-md bg-white/30 absolute top-0 left-0 right-0 bottom-0"></div>
+            </h1>
 
-        <button @click="changeDisplayBalance" type="button" class="btn btn-primary">
-            <Icon v-if="!displayBalance" name="lucide:eye" class="icon" />
-            <Icon v-else name="lucide:eye-closed" class="icon" />
-        </button>
-    </div>
+            <button @click="changeDisplayBalance" type="button" class="btn btn-primary">
+                <Icon v-if="!displayBalance" name="lucide:eye" class="icon" />
+                <Icon v-else name="lucide:eye-closed" class="icon" />
+            </button>
+        </div>
+    </ClientOnly>
 </template>
 
 <script setup lang="ts">
