37b20a3c2b0c9ba30ee509dd9c3feee894c29c36
felipeoli7eira <me.felipeoliveira@gmail.com>
2024-11-13 16:20:37 -0300

    Setting Dialog ClientOnly

--- app/components/Settings/index.vue
+++ app/components/Settings/index.vue
@@ -1,35 +1,37 @@
 <template>
-    <div class="settings-component">
-        <button class="btn btn-primary" type="button" onclick="settingsDialog.showModal()">
-            <Icon name="lucide:settings" class="icon" />
-        </button>
+    <ClientOnly>
+        <div class="settings-component">
+            <button class="btn btn-primary" type="button" onclick="settingsDialog.showModal()">
+                <Icon name="lucide:settings" class="icon" />
+            </button>
 
-        <dialog ref="settingsDialog" id="settingsDialog" class="modal">
-            <div class="modal-box">
-                <h3 class="text-lg font-bold mb-5">Configurações</h3>
-                <form @submit.prevent="onSubmit">
-                    <p class="mb-2">Defina a renda que será usada para calcular os gastos:</p>
-                    <input
-                        type="text"
-                        v-money="moneySettings"
-                        name="income"
-                        class="input input-bordered w-full"
-                        placeholder="Ex.: 1200,00"
-                        v-model="incomeModel"
-                    />
-                    <p class="text-red-500" v-if="incomeErrorMessage">{{ incomeErrorMessage }}</p>
+            <dialog ref="settingsDialog" id="settingsDialog" class="modal">
+                <div class="modal-box">
+                    <h3 class="text-lg font-bold mb-5">Configurações</h3>
+                    <form @submit.prevent="onSubmit">
+                        <p class="mb-2">Defina a renda que será usada para calcular os gastos:</p>
+                        <input
+                            type="text"
+                            v-money="moneySettings"
+                            name="income"
+                            class="input input-bordered w-full"
+                            placeholder="Ex.: 1200,00"
+                            v-model="incomeModel"
+                        />
+                        <p class="text-red-500" v-if="incomeErrorMessage">{{ incomeErrorMessage }}</p>
 
-                    <button type="submit" class="btn btn-primary w-full mt-5">Salvar</button>
-                </form>
-
-                <div class="modal-action">
-                    <form method="dialog">
-                        <button class="btn" type="submit">Fechar</button>
+                        <button type="submit" class="btn btn-primary w-full mt-5">Salvar</button>
                     </form>
+
+                    <div class="modal-action">
+                        <form method="dialog">
+                            <button class="btn" type="submit">Fechar</button>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </dialog>
-    </div>
+            </dialog>
+        </div>
+    </ClientOnly>
 </template>
 
 
